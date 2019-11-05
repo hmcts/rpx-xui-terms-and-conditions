e37c32cccbf7e6e0703e264e76bab065720c08ca
Philip <philip.aberneithy@gmail.com>
2019-11-05 17:34:11 +0000

    v1

--- server/routes.ts
+++ server/routes.ts
@@ -4,6 +4,6 @@ import copyManagementRouter from './api/controllers/copyManagement/router';
 import usersRouter from './api/controllers/users/router'
 
 export default function routes(app: Application): void {
-  app.use('/api/termsAndConditions', copyManagementRouter);
-  app.use('/api/termsAndConditions/:app/users', usersRouter);
+  app.use('/api/v1/termsAndConditions', copyManagementRouter);
+  app.use('/api/v1/termsAndConditions/:app/users', usersRouter);
 };
