6804c49788e2ea972d2d5e9b6821aa2daa62a23b
Philip <philip.aberneithy@gmail.com>
2019-11-07 16:50:22 +0000

    App health.

--- server/routes.ts
+++ server/routes.ts
@@ -6,4 +6,7 @@ import usersRouter from './api/controllers/users/router'
 export default function routes(app: Application): void {
   app.use('/api/v1/termsAndConditions', copyManagementRouter);
   app.use('/api/v1/termsAndConditions/:app/users', usersRouter);
+  app.get('/health', (req, res, next) => {
+    res.status(200).send('Terms and Conditions is Up')
+  })
 };
