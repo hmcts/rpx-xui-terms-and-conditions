4c233b7ec6568ac86eb51552be6da20f08defe66
Ernest Man <ernestman@emsys9.local>
2019-11-21 15:22:13 +0000

    update env config

--- server/index.ts
+++ server/index.ts
@@ -1,5 +1,8 @@
 import Server from './common/server';
 import routes from './routes';
+import * as config from 'config'
+import * as propertiesVolume from '@hmcts/properties-volume'
+propertiesVolume.addTo(config)
 
 export default new Server()
   .router(routes)
