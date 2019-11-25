acb80df64178cec2ae5aa3b0160c9bfdac70ef39
Pardeep Singh Basi <paddybasi@gmail.com>
2019-11-25 13:41:31 +0000

    test init containers

--- server/database/scripts/create_db.ts
+++ server/database/scripts/create_db.ts
@@ -3,24 +3,30 @@ import { db } from '../index';
 async function createSchema() {
     try {
         return await db.task('create-schema', async task => {
+            const apps = ['xuiwebapp', 'xuimowebapp'];
             const docsCreate = await task.documents.create();
             const appsCreate = await task.apps.create();
             const docAppsCreate = await task.documentApps.create();
             const userAgreementsCreate = await task.userAgreements.create();
             const appsInit = await task.apps.init();
-            /*try {
-                return await task.documents.add({
+            let docInsert: any = 'document exists';
+
+            // TODO: make this more efficient, also rather than total for both apps should be at least one for each
+            const totalDocs = async () => {
+                return Promise.all(apps.map(app => task.documents.total({ app })));
+            };
+            const totalArray = await totalDocs();
+
+            const total = totalArray.reduce( ((previousValue, currentValue) => previousValue + currentValue ));
+
+            if (total === 0) {
+                docInsert = await task.documents.add({
                     document: '<h1>Terms & Conditions</h1><p>Do you accept?</p>',
-                    mimeType: 'text/plain',
-                    apps: ['xuiwebapp', 'xuimowebapp']
+                    mimeType: 'text/html',
+                    apps: apps
                 });
-            } catch (e) {
-                console.log(e);
-            }*/
-            // return db.documents.findLatest({ app: 'xuiwebapp' });
-            // return db.documents.findByVersion({ app: 'xuiwebapp', version: 4 });
-            return db.documents.total({ app: 'xuimowebapp' });
-            // return { docsCreate, appsCreate, docAppsCreate, userAgreementsCreate, appsInit, docInsert };
+            }
+            return { docsCreate, appsCreate, docAppsCreate, userAgreementsCreate, appsInit, docInsert };
         });
     } catch (e) {
         console.log(e);
@@ -29,6 +35,5 @@ async function createSchema() {
 
 createSchema().then(result => {
     console.log(result);
-    // console.log('done, inserted %d rows', result.appsInit.rowCount);
     db.$pool.end();
 });
