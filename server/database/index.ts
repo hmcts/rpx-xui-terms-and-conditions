d365b35cc7f6baaa75a457f4b6eee3f22c3a5848
Pardeep Singh Basi <paddybasi@gmail.com>
2019-11-26 14:52:42 +0000

    lint fixes

--- server/database/index.ts
+++ server/database/index.ts
@@ -1,21 +1,19 @@
 import pgPromise = require('pg-promise'); // pg-promise core library
-import process from 'process';
-import {IInitOptions, IDatabase, IMain} from 'pg-promise';
+import { IInitOptions, IDatabase, IMain } from 'pg-promise';
 import {
     Extensions,
     TCAppRepository,
     TCDocumentAppRepository,
     TCDocumentRepository,
-    TCUserAgreementRepository
+    TCUserAgreementRepository,
 } from './repos';
-import {Diagnostics} from './diagnostics';
+import { Diagnostics } from './diagnostics';
 import config from 'config';
 
 export type ExtendedProtocol = IDatabase<Extensions> & Extensions;
 
 // pg-promise initialization options:
 const initOptions: IInitOptions<Extensions> = {
-
     // Extending the database protocol with our custom repositories;
     // API: http://vitaly-t.github.io/pg-promise/global.html#event:extend
     extend(obj: ExtendedProtocol, dc: any) {
@@ -27,26 +25,62 @@ const initOptions: IInitOptions<Extensions> = {
         obj.apps = new TCAppRepository(obj, pgp);
         obj.documentApps = new TCDocumentAppRepository(obj, pgp);
         obj.userAgreements = new TCUserAgreementRepository(obj);
-    }
+    },
 };
 
-const environmentDatabaseConfig = config => {
+/**
+ * environmentDatabaseConfig
+ *
+ * On the higher environments ie. AAT & Production these configuration values
+ * are coming from custom-environment-variables.yaml
+ *
+ * This is as per the Reform standard. [25.11.2019]
+ *
+ * @see customer-environment-variables.yaml
+ *
+ * @param config
+ * @returns
+ */
+const environmentDatabaseConfig = (config: config.IConfig) => {
     return {
         host: config.get<string>('database.host'),
-        port: <number>parseInt(config.get<string>('database.port'), 10),
+        port: parseInt(config.get<string>('database.port'), 10) as number,
         database: config.get<string>('database.name'),
         user: config.get<string>('database.username'),
         password: config.get<string>('secrets.rpx.postgresql-pw'),
-    }
+    };
 };
 
 // Initializing the library:
+// TODO: Remove from global scope
 const pgp: IMain = pgPromise(initOptions);
 
-// Creating the database instance with extensions:
-export const db: ExtendedProtocol = pgp(dbConfig);
+/**
+ * initialiseDatabase
+ *
+ * When we are running unit tests we do not want to connect to the Postgres database on
+ * our local machine, and on our Jenkins pipelines.
+ *
+ * Using the package.json scripts tag `test` we pass in UNIT_TEST_ENVIRONMENT=true
+ * which means when the Unit tests are run we do not attempt to connect to the db.
+ *
+ * Note that
+ * <code>Diagnostics.init(initOptions);</code>
+ * is used to allow database debugging.
+ *
+ * TODO: Place Diagnostics init into here.
+ *
+ * @param unitTestEnvironment ie. 'true'
+ * @returns {ExtendedProtocol | null}
+ */
+const initialiseDatabase = (unitTestEnvironment): ExtendedProtocol | null => {
+    if (unitTestEnvironment) {
+        return null;
+    }
 
-// Initializing optional diagnostics:
+    return pgp(environmentDatabaseConfig(config));
+};
+
+export const db: ExtendedProtocol = initialiseDatabase(process.env.UNIT_TEST_ENVIRONMENT);
+
 Diagnostics.init(initOptions);
-
-
