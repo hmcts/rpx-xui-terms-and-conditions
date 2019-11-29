e082f0bf102fb845121b5d0c2da589cb1082257b
Uday Denduluri <uday.denduluri@gmail.com>
2019-11-29 16:46:25 +0000

    Adding the Repository spec

---
+++ test/TCDocumentRepository.spec.ts
@@ -0,0 +1,60 @@
+import { TCDocumentRepository } from "../server/database/repos"
+
+describe('TC Document  Repository', () => {
+
+    it('should create', () => {
+
+        const db: any = {
+            none: jest.fn().mockReturnValue({}),
+        };
+
+        const tcDocumentDepository = new TCDocumentRepository(db);
+        tcDocumentDepository.create();
+        expect(db.none).toHaveBeenCalled();
+    })
+
+    it('should total', () => {
+        const db: any = {
+            one: jest.fn().mockReturnValue({}),
+        };
+        const tcDocumentDepository = new TCDocumentRepository(db);
+        tcDocumentDepository.total({ app: 'string' });
+        expect(db.one).toHaveBeenCalled();
+    })
+
+    it('should all', () => {
+        const db: any = {
+            any: jest.fn().mockReturnValue({}),
+        };
+        const tcDocumentDepository = new TCDocumentRepository(db);
+        tcDocumentDepository.all({ app: 'string' });
+        expect(db.any).toHaveBeenCalled();
+    })
+
+    it('should findbyVersion', () => {
+        const db: any = {
+            oneOrNone: jest.fn().mockReturnValue({}),
+        };
+        const tcDocumentDepository = new TCDocumentRepository(db);
+        tcDocumentDepository.findByVersion({ app: 'string', version: 123 });
+        expect(db.oneOrNone).toHaveBeenCalled();
+    })
+
+    it('should findLatest', () => {
+        const db: any = {
+            oneOrNone: jest.fn().mockReturnValue({}),
+        };
+        const tcDocumentDepository = new TCDocumentRepository(db);
+        tcDocumentDepository.findLatest({ app: 'string' });
+        expect(db.oneOrNone).toHaveBeenCalled();
+    })
+
+    it('should find', () => {
+        const db: any = {
+            oneOrNone: jest.fn().mockReturnValue({}),
+        };
+        const tcDocumentDepository = new TCDocumentRepository(db);
+        tcDocumentDepository.find({ documentId: 123});
+        expect(db.oneOrNone).toHaveBeenCalled();
+    })
+});
