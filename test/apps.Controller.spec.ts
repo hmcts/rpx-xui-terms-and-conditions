80c7a5e055124a796220c2eeebc25c25a08550cd
Pardeep Singh Basi <paddybasi@gmail.com>
2019-11-29 13:15:48 +0000

    fixing tests [WIP]

--- test/apps.Controller.spec.ts
+++ test/apps.Controller.spec.ts
@@ -1,5 +1,6 @@
 import AppsController from '../server/api/controllers/apps/appsController';
 import AppsService from '../server/api/services/apps.service';
+import {jestPreset} from "ts-jest";
 
 /**
  * Mock Express Request Object using Jest
@@ -25,29 +26,33 @@ const mockResponse = () => {
     };
 };
 
+const mockNext = () => {
+    return jest.fn();
+}
+
 describe('Apps Controller', () => {
-
     describe('allApps()', () => {
-        it('should throw a 500 if we\'re not able to hit the database.', async () => {
-
+        it("should throw a 500 if we're not able to hit the database.", async () => {
             const req = mockRequest();
             const res = mockResponse();
+            const next = mockNext();
 
-            await AppsController.allApps(req as any, res as any);
+            await AppsController.allApps(req as any, res as any, next as any);
 
             expect(res.status).toHaveBeenCalledWith(500);
         });
-        
+
         it('should return a 200 Success and return the response data, if AppsService.allApps() returns response data.', async () => {
             const RESPONSE_DATA: any = {
                 response: 42,
             };
             const req = mockRequest();
             const res = mockResponse();
+            const next = mockNext();
 
             const spy = jest.spyOn(AppsService, 'getAllApps').mockImplementation(() => Promise.resolve(RESPONSE_DATA));
 
-            await AppsController.allApps(req as any, res as any);
+            await AppsController.allApps(req as any, res as any, next as any);
             expect(res.status).toHaveBeenCalledWith(200);
             expect(res.status().send).toHaveBeenCalledWith(RESPONSE_DATA);
         });
