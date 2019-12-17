0b29898d87b12cc54fe4da16ec5c6a793acc5dee
Pardeep Singh Basi <paddybasi@gmail.com>
2019-12-17 16:38:58 +0000

    incorrect connection params

--- server/database/scripts/migrate.ts
+++ server/database/scripts/migrate.ts
@@ -2,10 +2,14 @@ import { migrate, MigrateDBConfig } from 'postgres-migrations';
 import { environmentDatabaseConfig } from '../index';
 import config from 'config';
 import * as path from 'path';
-
-console.log(environmentDatabaseConfig);
+import pg from 'pg';
 
 export const MIGRATIONS_PATH = path.join(__dirname, '../migrations');
+
+// check whether to use SSL
+if (config.has('database.ssl') && JSON.parse(config.get('database.ssl'))) {
+    pg.defaults.ssl = true;
+}
 
 migrate((environmentDatabaseConfig(config) as any) as MigrateDBConfig, MIGRATIONS_PATH, {
     logger: msg => console.log(msg),
