b3f046286279fcd40676b2c11e56b19ee9674d3f
Philip <philip.aberneithy@gmail.com>
2020-01-16 16:40:49 +0000

    Check if yarn migrate runs.

--- server/database/scripts/migrate.ts
+++ server/database/scripts/migrate.ts
@@ -1,17 +1,25 @@
-import { migrate, MigrateDBConfig } from 'postgres-migrations';
-import { db } from '../index';
+import {migrate, MigrateDBConfig} from 'postgres-migrations';
+import {db} from '../index';
 import * as path from 'path';
 
 export const MIGRATIONS_PATH = path.join(__dirname, '../migrations');
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-migrate(({ client: db.$pool as any } as unknown) as MigrateDBConfig, MIGRATIONS_PATH, {
-    logger: msg => console.log(msg),
-})
-    .then(() => {
-        console.log('migrate complete');
-        db.$pool.end();
-    })
-    .catch(err => {
-        console.log('migrations failed', err);
-    });
+
+const startMigration = () => {
+
+    if (process.env.NODE_CONFIG_ENV === 'preview') {
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        migrate(({client: db.$pool as any} as unknown) as MigrateDBConfig, MIGRATIONS_PATH, {
+            logger: msg => console.log(msg),
+        })
+            .then(() => {
+                console.log('migrate complete');
+                db.$pool.end();
+            })
+            .catch(err => {
+                console.log('migrations failed', err);
+            });
+    }
+}
+
+startMigration();
