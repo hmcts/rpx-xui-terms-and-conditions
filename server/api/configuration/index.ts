46df28e1e9e2963494fb7fa111b6ad1c192e7fc8
Philip a <philip.aberneithy@gmail.com>
2020-01-30 11:27:10 +0000

    Linting issues.

--- server/api/configuration/index.ts
+++ server/api/configuration/index.ts
@@ -28,7 +28,6 @@ export const getEnvironment = () => process.env.NODE_CONFIG_ENV;
  * @returns {string}
  */
 export const getPostgresSecret = (secretsConfig, useKeyVaultSecret): string => {
-
     const ERROR_POSTGRES_SECRET_NOT_FOUND =
         'secrets.rpx.postgresql-admin-pw not found on this environment, therefore using the password in the .yaml' +
         'file for this environment.';
