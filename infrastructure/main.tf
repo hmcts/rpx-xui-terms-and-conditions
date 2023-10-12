14e04d5461accdd92809121eca4b414b45bb3cc2
Andy Wilkins <49269487+andywilkinshmcts@users.noreply.github.com>
2023-10-12 12:27:23 +0100

    Update main.tf

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -2,42 +2,6 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "rg" {
-  name     = "${var.product}-${var.component}-${var.env}"
-  location = var.location_app
-}
-
-locals {
-  local_env = "${(var.env == "preview" || var.env == "spreview") ? (var.env == "preview" ) ? "aat" : "saat" : var.env}"
-
-  shared_vault_name = "${var.shared_product_name}-${local.local_env}"
-}
-
-module "db" {
-  source                = "git@github.com:hmcts/cnp-module-postgres?ref=master"
-  product               = "${var.product}-${var.component}"
-  location              = var.location_db
-  env                   = var.env
-  database_name         = var.database_name
-  postgresql_user       = var.postgresql-admin-username
-  sku_name              = var.sku_name
-  sku_capacity          = var.capacity
-  postgresql_version    = var.postgresql_version
-  sku_tier              = var.sku_tier
-  common_tags           = var.common_tags
-  subscription          = var.subscription
-}
-
-data "azurerm_key_vault" "key_vault" {
-    name = local.shared_vault_name
-    resource_group_name = local.shared_vault_name
-}
-
-resource "azurerm_key_vault_secret" "postgresql-pw" {
-  key_vault_id = data.azurerm_key_vault.key_vault.id
-  name         = "postgresql-admin-pw"
-  value        = module.db.postgresql_password
-}
 
 
 
